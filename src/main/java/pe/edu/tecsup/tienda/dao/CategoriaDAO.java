859d96a55329c1db909efbb71501abdb8bebcf08
jgomezz <jgomezz@gmail.com>
2025-12-14 13:05:52 -0500

    update

---
+++ src/main/java/pe/edu/tecsup/tienda/dao/CategoriaDAO.java
@@ -0,0 +1,11 @@
+package pe.edu.tecsup.tienda.dao;
+
+import java.util.List;
+
+import pe.edu.tecsup.tienda.entidad.Categoria;
+
+public interface CategoriaDAO {
+
+    List<Categoria> obtenerCategorias();
+
+}
---
+++ src/main/java/pe/edu/tecsup/tienda/dao/CategoriaDAOImpl.java
@@ -0,0 +1,68 @@
+package pe.edu.tecsup.tienda.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import pe.edu.tecsup.tienda.entidad.Categoria;
+import pe.edu.tecsup.tienda.utils.ConexionBD;
+
+public class CategoriaDAOImpl implements CategoriaDAO {
+
+    @Override
+    public List<Categoria> obtenerCategorias() {
+
+        List<Categoria> categorias = new ArrayList<Categoria>();
+
+        try {
+
+            // Conectarme a la Base de datos
+            Connection con = ConexionBD.obtenerConexion();
+
+            // Preparar la sentencia
+            String sql= """
+                            SELECT * FROM categorias
+                        """;
+            PreparedStatement stmt= con.prepareStatement(sql);
+
+            ResultSet rs = stmt.executeQuery();
+
+            while(rs.next()) {
+
+                // Lee informacion de un registro de la tabla categorias
+                int id= rs.getInt("id");
+                String nombre = rs.getString("nombre");
+                String descripcion = rs.getString("descripcion");
+                String orden = rs.getString("orden");
+
+                // Crea un objeto Categoria
+                Categoria categoria = new Categoria();
+                categoria.setId(id);
+                categoria.setNombre(nombre);
+                categoria.setDescripcion(descripcion);
+                categoria.setOrden(orden);
+
+                // Agrega el objeto a la lista
+                categorias.add(categoria);
+            }
+
+            rs.close();
+            stmt.close();
+            con.close();
+
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        return categorias;
+
+    }
+
+}
