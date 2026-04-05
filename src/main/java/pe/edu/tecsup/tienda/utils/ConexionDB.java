1096b16e210e72500132a79197139f7b7a11c6d9
jgomezz <jgomezz@gmail.com>
2026-04-05 12:29:02 -0500

    create ConexionDB.java

---
+++ src/main/java/pe/edu/tecsup/tienda/utils/ConexionDB.java
@@ -0,0 +1,25 @@
+package pe.edu.tecsup.tienda.utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ * 
+ */
+public class ConexionDB {
+	
+	final static String URL = "jdbc:mariadb://localhost/tienda";
+    final static String USER = "root";
+    final static String PASSWORD = "root";
+
+    public static Connection obtenerConexion() throws Exception {
+
+        // Cargar el driver
+        Class.forName("org.mariadb.jdbc.Driver");
+
+        // Conectarme a la Base de datos
+        Connection con = DriverManager.getConnection(URL,USER,PASSWORD);
+
+        return con;
+    }
+}
