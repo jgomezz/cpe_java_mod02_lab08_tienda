a7321bbe6f948ac51aaf93f01ba2b290f54c60c5
jgomezz <jgomezz@gmail.com>
2026-04-05 12:31:13 -0500

    create Categoria and Producto

---
+++ src/main/java/pe/edu/tecsup/tienda/entities/Categoria.java
@@ -0,0 +1,49 @@
+package pe.edu.tecsup.tienda.entities;
+
+public class Categoria {
+
+	private Integer id;
+    private String nombre;
+    private String descripcion;
+    private String orden;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getOrden() {
+        return orden;
+    }
+
+    public void setOrden(String orden) {
+        this.orden = orden;
+    }
+
+	@Override
+	public String toString() {
+		return "Categoria [id=" + id + ", nombre=" + nombre + ", descripcion=" + descripcion + ", orden=" + orden + "]";
+	}
+    
+    
+    
+}
