89be4e8a9f62751651e82155f160349c1c035c21
jgomezz <jgomezz@gmail.com>
2026-04-19 12:42:34 -0500

    Agregando soporte del pool de conexiones a base de datos

--- src/main/java/pe/edu/tecsup/tienda/utils/ConexionBD.java
+++ src/main/java/pe/edu/tecsup/tienda/utils/ConexionBD.java
@@ -1,13 +1,18 @@
 package pe.edu.tecsup.tienda.utils;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
 
 /**
  * 
  */
 public class ConexionBD {
+
 	
+	/*
 	final static String URL = "jdbc:mariadb://localhost/tienda";
     final static String USER = "root";
     final static String PASSWORD = "root";
@@ -22,4 +27,31 @@ public class ConexionBD {
 
         return con;
     }
+	 */
+
+    public static Connection obtenerConexion() throws Exception {
+    
+    	Connection con = null;
+		try {
+			
+			Context initContext = new InitialContext();
+			Context envContext = (Context) initContext.lookup("java:/comp/env");
+		
+			
+			DataSource datasource = (DataSource) envContext.lookup("jdbc/tienda");
+		
+			
+			con = datasource.getConnection();
+		
+		} catch (NamingException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return con;
+		
+    }
+    	
+	
 }
