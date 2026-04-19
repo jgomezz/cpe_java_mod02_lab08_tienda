1071dc02c446089c5ac85aad47a8f1b813a661e8
jgomezz <jgomezz@gmail.com>
2026-04-19 08:49:51 -0500

    update producto repository

--- src/main/java/pe/edu/tecsup/tienda/repositories/ProductoRepositoryImpl.java
+++ src/main/java/pe/edu/tecsup/tienda/repositories/ProductoRepositoryImpl.java
@@ -1,12 +1,15 @@
 package pe.edu.tecsup.tienda.repositories;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.log4j.Logger;
 
+import pe.edu.tecsup.tienda.entities.Categoria;
 import pe.edu.tecsup.tienda.entities.Producto;
 import pe.edu.tecsup.tienda.utils.ConexionBD;
 
@@ -27,7 +30,31 @@ public class ProductoRepositoryImpl implements ProductoRepository {
             // Conectarme a la Base de datos
             Connection con = ConexionBD.obtenerConexion();
         
-            // TODO
+            // Preparar la sentencia
+            String sql= 
+            		"""
+                    // TODO   
+                        
+                    """;
+            PreparedStatement stmt= con.prepareStatement(sql);
+
+            ResultSet rs = stmt.executeQuery();
+
+            while(rs.next()) {
+
+                // Lee informacion de un registro de la tabla productos
+                
+            	Producto producto = new Producto();
+            	
+            	// TODO
+
+                // Agrega el objeto a la lista
+            	productos.add(producto);
+            }
+
+            rs.close();
+            stmt.close();
+            con.close();
             
             
         } catch (ClassNotFoundException e) {
@@ -37,7 +64,6 @@ public class ProductoRepositoryImpl implements ProductoRepository {
         } catch (Exception e) {
             throw new RuntimeException(e);
         }     
-            
         
 		return productos;
 	}
